261a3849dccabee52eab89a93c23453ef3f8f74d
ajari-799 <114122236+ajari-799@users.noreply.github.com>
2022-09-22 12:34:52 -0700

    added msges

--- stock_plotter.py
+++ stock_plotter.py
@@ -1,8 +1,11 @@
-inputx=input("Enter stock plotter mode:")
+inputx=str(input("Enter stock plotter mode:"))
 y=0
 x=len(inputx)
-while y<x:
-    inputx[y]="vertical"
+while y<=x:
+    inputx[y]=='vertical'
     y+=1
 print(y)
-
+print(y)
+print(y)
+print(y)
+print(y)
